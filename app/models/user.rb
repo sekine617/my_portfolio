f909eb7ec3b65b51f9ffec54d7775380bc6ffcb1
sekine617 <seweb14wm54@icloud.com>
2020-12-25 15:09:27 +0900

    add user.rb validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,35 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    has_secure_password
+
+      validates :last_name,presence: true,
+
+      validates :first_name,presence: true,
+
+      validates :last_hurigana,
+      presence: true,
+      format: { 
+          with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+          message: 'はカタカナで入力して下さい。'
+              }
+
+      validates :first_hurigana,
+      presence: true,
+      format: { 
+        with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+        message: 'はカタカナで入力して下さい。'
+            }
+
+      validates :password,
+      length: { minimum: 8 }
+
+      validates :email,
+      presence: true,
+      uniqueness: true,
+
+      validates :phone_number,
+      presence: true,
+      numericality: { only_integer: true }
+      length: { minimum: 10 }
 end
