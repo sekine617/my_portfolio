ec8d928e0e9ee5e087da06998589cc77e1a2658d
sekine617 <seweb14wm54@icloud.com>
2020-12-25 14:39:52 +0900

    add userRouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'mypage' => 'users#me'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resource :users, only: %i[new create]
 end
