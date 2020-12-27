d7504a74820bc00096a1447f3bfa91b5bb8303f9
sekine617 <seweb14wm54@icloud.com>
2020-12-27 22:27:13 +0900

    add listController add productModel

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :users, only: %i[new create]
+  resources :lists, only: %i[index new]
 end
