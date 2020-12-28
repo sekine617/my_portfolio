421e26a766d0f5429dc0cf3f48191b3a9b2aab2c
sekine617 <seweb14wm54@icloud.com>
2020-12-28 13:08:48 +0900

    add pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = params[:tag_id].present? ? Tag.find(params[:tag_id]).products : Product.all
-    @products = @products.page(params[:page])
+    #@products = params[:tag_id].present? ? Tag.find(params[:tag_id]).products : Product.all
+    @products = Product.page(params[:page])
   end
 
   def new
