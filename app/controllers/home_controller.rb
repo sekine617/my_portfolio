e7e992513d5a13cc9fed5d523889f21682b0503d
sekine617 <seweb14wm54@icloud.com>
2021-01-25 20:20:26 +0900

    modify style homeindex

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @latest_products = Product.all.order(id: "DESC").limit(3)
     @popular_products_all = Product.includes(:order_products).sort {|a,b| b.order_products.size <=> a.order_products.size}
     @popular_products = @popular_products_all[0..2]
+    @tag_results = Product.tagged_with("チョコレート").includes(:order_products).sort {|a,b| b.order_products.size <=> a.order_products.size}
+    @chocolate_products = @tag_results[0..2]
   end
 end
